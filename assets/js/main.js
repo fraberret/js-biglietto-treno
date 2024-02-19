57fb42b366b8252f129e695807c751198d636b61
fraberret <f.berretta02@gmail.com>
2024-02-19 16:27:29 +0100

    fix calc of discount and finish exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,26 +3,32 @@ let userAge = prompt("quanti anni hai?")
 let userKm = prompt("quanti km vuoi percorrere?")
 //calcolare prezzo
 // in base ai km
-let kmPrice = (userKm * 0.21)
+let ticket = (userKm * 0.21)
 //in base all'età
 //SE utente ha < 18 anni -20%
 //ALTRIMENTI SE > 65 anni -40%
 //ALTRIMENTI no sconto
-let prezzoBiglietto
+
+let finalPrice=ticket//prezzo ticket base
 
 if (userAge < 18) { //se minorenne
     console.log(userAge, userKm);
-    let prezzoBiglietto = userKm * 20 / 100
-    console.log(prezzoBiglietto);
+    let sconto = (ticket * 20 / 100)
+    let finalPrice = (ticket - sconto)
+    console.log(finalPrice.toFixed(2));//prezzo finale
 } else if (userAge > 65) { //se over 65
     console.log(userAge, userKm);
-    let prezzoBiglietto = userKm * 40 / 100
-    console.log(prezzoBiglietto);
+    let sconto = (ticket * 40 / 100)
+    let finalPrice = (ticket - sconto)
+    console.log(finalPrice.toFixed(2));//prezzo finale
 } else{ //se nessuno dei due
     console.log(userAge, userKm);
-    prezzoBiglietto = kmPrice
-    console.log(prezzoBiglietto);
+    console.log(finalPrice.toFixed(2));//prezzo finale
 }
+
+
+
+
 
 
 
