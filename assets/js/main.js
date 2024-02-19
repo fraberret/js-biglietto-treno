35a9ec49407b177a79f4a8e81e1fd4a92c005aec
fraberret <f.berretta02@gmail.com>
2024-02-19 15:53:29 +0100

    add price if under 18, over 65 or normal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,30 @@
 //chiedere all'utente età e chilometri
-let eta = prompt("quanti anni hai?")
-let chilometri = prompt("quanti km vuoi percorrere?")
+let userAge = prompt("quanti anni hai?")
+let userKm = prompt("quanti km vuoi percorrere?")
 //calcolare prezzo
+// in base ai km
+let kmPrice = (userKm * 0.21)
+//in base all'età
+//SE utente ha < 18 anni -20%
+//ALTRIMENTI SE > 65 anni -40%
+//ALTRIMENTI no sconto
+let prezzoBiglietto
 
-//stampare il risultato
+if (userAge < 18) { //se minorenne
+    console.log(userAge, userKm);
+    let prezzoBiglietto = userKm * 20 / 100
+    console.log(prezzoBiglietto);
+} else if (userAge > 65) { //se over 65
+    console.log(userAge, userKm);
+    let prezzoBiglietto = userKm * 40 / 100
+    console.log(prezzoBiglietto);
+} else{ //se nessuno dei due
+    console.log(userAge, userKm);
+    prezzoBiglietto = kmPrice
+    console.log(prezzoBiglietto);
+}
+
+
+
+
+
